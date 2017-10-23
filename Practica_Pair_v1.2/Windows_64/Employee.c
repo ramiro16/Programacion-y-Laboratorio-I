ff518664685e19898f25276d55119106ab13040f
ramiro16 <ramirotapia16@gmail.com>
2017-10-23 12:26:21 -0300

    Add files via upload

---
+++ Practica_Pair_v1.2/Windows_64/Employee.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+
+int employee_compare(void* pEmployeeA,void* pEmployeeB)
+{
+    return 0;
+}
+
+
+void employee_print(Employee* this)
+{
+
+    printf("%d---%s---%s---%d\n",this->id,this->name,this->lastName,this->isEmpty);
+
+}
+
+
+Employee* employee_new(int id, char* nombre, char* apellido, int estado)
+{
+    Employee* pEmpleado = (Employee*)malloc(sizeof(Employee));
+
+    if(pEmpleado!=NULL)
+    {
+        pEmpleado->id = id;
+        strcpy(pEmpleado->name, nombre);
+        strcpy(pEmpleado->lastName, apellido);
+        pEmpleado->isEmpty = estado;
+    }
+
+    return pEmpleado;
+
+}
+
+void employee_delete(Employee* this)
+{
+
+
+}
+
+int employee_setId(Employee* this, int id)
+{
+
+    return 0;
+
+}
+
+int employee_getId(Employee* this)
+{
+
+    return 0;
+
+}
+
+
---
+++ Practica_Pair_v1.2/Windows_64/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+/****************************************************
+    Menu:
+        1. Parse del archivo data.csv
+        2. Listar Empleados
+        3. Ordenar por nombre
+        4. Agregar un elemento
+        5. Elimina un elemento
+        6. Listar Empleados (Desde/Hasta)
+*****************************************************/
+
+
+int main()
+{
+    int i;
+    Employee* auxEmpleado;
+    ArrayList* lista = al_newArrayList();
+
+    Employee* empleado1;
+    Employee* empleado2;
+    Employee* empleado3;
+
+    empleado1=employee_new(1,"Maria","Sanchez",1);
+    empleado2=employee_new(2,"Pedro","Gomez",1);
+    empleado3=employee_new(3,"Ignacio","Ferreyra",1);
+
+    lista->add(lista,empleado1);
+    lista->add(lista,empleado2);
+    lista->add(lista,empleado3);
+
+    for(i=0;i<lista->len(lista);i++)
+    {
+        auxEmpleado = (Employee*) lista->get(lista,i);
+        employee_print(auxEmpleado);
+    }
+
+    return 0;
+}
